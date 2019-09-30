29331e1d6a698a0a6d42e3545fcf045205be1e9c
Magnus Kühn <magnus.khn@gmail.com>
2019-09-30 11:43:16 +0200

    Implement compute_normalized_angle with tests

--- game/direction.py
+++ game/direction.py
@@ -1,17 +1,34 @@
-# directions:
-# - right 0
-# - top 1
-# - left 2
-# - down 3
+import math
+from typing import Tuple
+
+
+class Directions:
+    RIGHT = 0
+    UP = 1
+    LEFT = 2
+    DOWN = 3
+
+
+def compute_normalized_angle(direction: int, snake: Tuple[int, int], apple: Tuple[int, int]) -> float:
+    """ Computes the angle to the apple
+        The angle is relative to the current movement direction and normalized to [-1, 1]
+    """
+    # compute atan2 with apple rotated around snake for down direction
+    if direction == Directions.UP:
+        angle = math.atan2(-(apple[0] - snake[0]), -(apple[1] - snake[1]))
+    else:
+        angle = math.atan2(apple[0] - snake[0], apple[1] - snake[1])
+
+    return angle / math.pi
 
 
 def direction_apply(direction, pos):
     (x, y) = pos
-    if direction == 0:
+    if direction == Directions.RIGHT:
         return x + 1, y
-    if direction == 1:
+    if direction == Directions.UP:
         return x, y - 1
-    if direction == 2:
+    if direction == Directions.LEFT:
         return x - 1, y
-    if direction == 3:
+    if direction == Directions.DOWN:
         return x, y + 1
--- game/simulation.py
+++ game/simulation.py
@@ -1,7 +1,19 @@
+import math
 from typing import Callable
 
 from game.data import Snake, Apple
 from render import SimpleHandler
+
+
+def dnn_to_handler(model) -> Callable[[Snake, Apple], int]:
+    def dnn_handler(snake: Snake, apple: Apple) -> int:
+        # compute angle to fruit
+        snake_x, snake_y = snake.pos[0]
+        angle = math.atan2(apple.pos[0] - snake_x, apple.pos[1] - snake_y)
+
+        pass
+
+    return dnn_handler
 
 
 def compute_score(step_handler: Callable[[Snake, Apple], int]) -> int:
