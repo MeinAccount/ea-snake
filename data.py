71cb32abf94d830b92458908dcfdb1a01b74b217
Magnus Kühn <magnus.khn@gmail.com>
2019-09-30 10:10:56 +0200

    Split code

---
+++ data.py
@@ -0,0 +1,64 @@
+import random
+
+GRID_WIDTH = 50
+GRID_HEIGHT = 50
+
+
+# directions:
+# - right 0
+# - top 1
+# - left 2
+# - down 3
+
+class Apple:
+    pos = (0, 0)
+    direction = 0
+
+    def __init__(self) -> None:
+        self.replace()
+
+    def replace(self):
+        self.pos = (random.randint(0, GRID_WIDTH), random.randint(0, GRID_HEIGHT))
+
+    def draw(self, surface, image):
+        surface.blit(image, (self.pos[0] * 10, self.pos[1] * 10))
+
+
+def direction_apply(direction, pos):
+    (x, y) = pos
+    if direction == 0:
+        return x + 1, y
+    if direction == 1:
+        return x, y - 1
+    if direction == 2:
+        return x - 1, y
+    if direction == 3:
+        return x, y + 1
+
+
+class Snake:
+    pos = []
+    current_direction = 0
+
+    def __init__(self, start_pos, length=3) -> None:
+        self.length = length
+        self.pos.append(start_pos)
+        for i in range(1, length):
+            self.pos.append(direction_apply(2, self.pos[i - 1]))
+
+    def draw(self, surface, image):
+        for (x, y) in self.pos:
+            surface.blit(image, (x * 10, y * 10))
+
+    def move(self, apple: Apple) -> bool:
+        new_pos = direction_apply(self.current_direction, self.pos[0])
+        if apple.pos == new_pos:
+            apple.replace()
+        else:
+            self.pos.pop()
+
+        if new_pos in self.pos:
+            return False  # self intersection!
+
+        self.pos.insert(0, new_pos)
+        return True
--- game.py
+++ game.py
@@ -1,73 +1,10 @@
-import random
 import time
 from typing import Callable
 
 import pygame
 from pygame.constants import K_ESCAPE
 
-# directions:
-# - right 0
-# - top 1
-# - left 2
-# - down 3
-
-
-GRID_WIDTH = 50
-GRID_HEIGHT = 50
-
-
-class Apple:
-    pos = (0, 0)
-    direction = 0
-
-    def __init__(self) -> None:
-        self.replace()
-
-    def replace(self):
-        self.pos = (random.randint(0, GRID_WIDTH), random.randint(0, GRID_HEIGHT))
-
-    def draw(self, surface, image):
-        surface.blit(image, (self.pos[0] * 10, self.pos[1] * 10))
-
-
-def direction_apply(direction, pos):
-    (x, y) = pos
-    if direction == 0:
-        return x + 1, y
-    if direction == 1:
-        return x, y - 1
-    if direction == 2:
-        return x - 1, y
-    if direction == 3:
-        return x, y + 1
-
-
-class Snake:
-    pos = []
-    current_direction = 0
-
-    def __init__(self, start_pos, length=3) -> None:
-        self.length = length
-        self.pos.append(start_pos)
-        for i in range(1, length):
-            self.pos.append(direction_apply(2, self.pos[i - 1]))
-
-    def draw(self, surface, image):
-        for (x, y) in self.pos:
-            surface.blit(image, (x * 10, y * 10))
-
-    def move(self, apple: Apple) -> bool:
-        new_pos = direction_apply(self.current_direction, self.pos[0])
-        if apple.pos == new_pos:
-            apple.replace()
-        else:
-            self.pos.pop()
-
-        if new_pos in self.pos:
-            return False  # self intersection!
-
-        self.pos.insert(0, new_pos)
-        return True
+from data import Snake, Apple, GRID_HEIGHT, GRID_WIDTH
 
 
 class App:
@@ -121,8 +58,9 @@ class App:
 
             if keys[K_ESCAPE]:
                 running = False
+            else:
+                running = self.on_loop()
 
-            running = self.on_loop()
             self.on_render()
             time.sleep(50.0 / 1000.0)
         pygame.quit()
